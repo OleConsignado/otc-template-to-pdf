af6a379ef3303ecbeef1f3d45a06ef102064c037
Henrique Rodrigues Araujo <henriqueraraujo@gmail.com>
2018-08-27 16:00:00 -0300

    Adicionado a validação da variavel dados nula

--- source/Otc.TemplateToPdf/Conversor.cs
+++ source/Otc.TemplateToPdf/Conversor.cs
@@ -32,6 +32,7 @@ namespace ConverterObjectToPDF
         public byte[] ConverterTemplate(Dictionary<string, string> dados, string caminhoTemplate, List<object> imagens)
         {
             this.CarregarTemplate(caminhoTemplate);
+            if (dados == null) 
                 throw new ArgumentNullException(nameof(dados));
             if (dados.Count != this.Template.Parametros.Count)
                 throw new ArgumentOutOfRangeException("Número de parâmetros de entrada de dados diferente do número de parâmetros do template");
